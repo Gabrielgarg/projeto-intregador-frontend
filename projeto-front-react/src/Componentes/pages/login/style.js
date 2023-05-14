8a4c63a4dbafac0a5137fc4b64827f18c74a53fb
Gabriel Garuthi <gabrieldrmg@hotmail.com>
2023-05-14 17:00:14 -0300

    Atualizado

---
+++ projeto-front-react/src/Componentes/pages/login/style.js
@@ -0,0 +1,59 @@
+import { styled } from "styled-components";
+
+export const Modelspageoflogin = styled.body`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
+
+export const Divofpagelogin = styled.body`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  justify-items: center;
+  /* font-family: "IBM Plex Sans"; */
+  margin-top: 100px;
+`;
+
+export const Divabovelogin = styled.div`
+  justify-items: center;
+  margin-bottom: 100px;
+`;
+
+export const Divofimageslogin = styled.image`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  height: 83px;
+`;
+
+export const Divofinputlogin = styled.div`
+  width: 363px;
+`;
+
+export const Buttonlogin = styled.button`
+  margin-top: 60px;
+  font-family: "IBM Plex Sans";
+  background: linear-gradient(#ff6489, #f9b24e);
+  width: 365px;
+  border-radius: 40px;
+  border-color: white;
+  height: 50px;
+  color: white;
+`;
+
+export const Linelogin = styled.div`
+  margin-top: 20px;
+  width: 363px;
+  border: 1px solid #ff6489;
+`;
+
+export const Buttonlogin2 = styled.button`
+  margin-top: 20px;
+  background-color: white;
+  width: 365px;
+  border-radius: 40px;
+  border-color: #fe7e02;
+  height: 50px;
+  color: #fe7e02;
+`;
